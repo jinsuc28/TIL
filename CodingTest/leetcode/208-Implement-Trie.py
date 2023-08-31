b27eedb464467a222bf9423564ebb54eb0b317a5
jinsuc28 <jinsuc28@gmail.com>
2023-08-31 16:17:18 +0900

    add: TrieNode solution

--- CodingTest/leetcode/208-Implement-Trie.py
+++ CodingTest/leetcode/208-Implement-Trie.py
@@ -19,7 +19,40 @@ class Trie:
         return False
                 
         
+"""
+트라이 구현
 
+class TrieNode:
+    def __init__(self):
+        self.word = False
+        self.children = collections.defaultdict(TrieNode)
+
+class Trie:
+    def __init__(self):
+        self.word = False
+        self.root = collections.defaultdict(TrieNode())
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            node = node.children[char]
+        node.word = True
+    
+    def search(self, word: str) -> bool:
+        node = self.root
+        for char in word:
+            if char not in node.children: return False
+            node = node.children[char]
+        return node.word
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for char in prefix:
+            if char not in node.children: return False
+            node = node.children[char]
+        return True
+
+"""
 
 # Your Trie object will be instantiated and called as such:
 # obj = Trie()x``
