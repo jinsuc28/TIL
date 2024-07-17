b129e432eb39f2207307ad7d4b6fe0504272ecb8
jinsuc28 <jinsuc28@gmail.com>
2024-07-17 19:56:43 +0900

    delete

--- SQL/LeetCode/Easy/1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
+++
@@ -1,6 +0,0 @@
-SELECT r.contest_id, 
-        ROUND((COUNT(r.user_id)/(SELECT COUNT(UNIQUE(user_id)) 
-                            FROM Users))*100, 2) percentage 
-FROM Register r
-GROUP BY r.contest_id;
-ORDER BY percentage DESC, r.contest_id ASC;
