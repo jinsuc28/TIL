bbb72eb818df3c88c715b62436f231bb2173de5b
jinsuc28 <jinsuc28@gmail.com>
2024-07-18 13:15:22 +0900

    [medium] 1174. Immediate Food Delivery II

--- SQL/1174.ImmediateFoodDeliveryII.sql
+++ SQL/1174.ImmediateFoodDeliveryII.sql
@@ -1,12 +1,12 @@
-select ROUND((SUM(CASE tbl.MIN_ORDER_DATE
+SELECT ROUND((SUM(CASE tbl.MIN_ORDER_DATE
                        WHEN tbl.CUSTOMER_PREF_DELIVERY_DATE THEN 1 
                        ELSE 0 
                   END )/ COUNT(*))*100, 2) immediate_percentage
-from (select *
-      from (select d.customer_id, 
-                   min(d.order_date) min_order_date
-      from Delivery d
-      group by d.customer_id) min_order_tbl, 
+FROM (SELECT *
+      FROM (SELECT d.customer_id, 
+                   MIN(d.order_date) min_order_date
+      FROM Delivery d
+      GROUP BY d.customer_id) min_order_tbl, 
       Delivery d1
-      where min_order_tbl.min_order_date = d1.order_date
-      and min_order_tbl.customer_id = d1.customer_id) tbl
+      WHERE min_order_tbl.min_order_date = d1.order_date
+      AND min_order_tbl.customer_id = d1.customer_id) tbl
