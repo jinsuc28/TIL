f6febb5055a488f02b99e0919a71a584e46e6b3d
JS Choi <jinsuc28@gmail.com>
2024-08-05 15:39:54 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        boolean b = false;
+        
+        for (int i=0; i<10; i++){
+            b = false;
+            for (int num:numbers){
+                if (num == i){
+                    b =true;
+                }
+            }
+            if (b==false){
+                answer +=i;
+            }
+        }
+        
+        return answer;
+    }
+}
